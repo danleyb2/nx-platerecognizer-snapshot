9faa2fcfd69094a032942650c7e1b121d04b907d
Nyaundi Brian <danleyb2@gmail.com>
2021-09-20 16:37:38 +0300

    wip

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# nix-platerecognizer-snapshot
+# NX VMS + Platerecognizer Snapshot
+
+Generate thumbnails from motion events on NX VMS and send to Platerecognizer Plate Reader
+
+
+## Setup 
+
+### 1. Update configs at the top of main.py
+
+From NX VMS Server
+```python
+NX_API_BASE = 'https://192.168.0.14:7001/'
+NX_CAMERA_ID = '420f37f6-8875-6885-9200-11504e61f485'
+NX_LOGIN = "admin"
+NX_PASSWORD = "############"
+```
+
+From Platerecognizer
+```python
+API_TOKEN = '4805bee1222########################################'
+```
+
+### 3. Install requirements.txt and Run script
+```bash
+python main.py
+```
+
+### 2. Configure Camera Rules
+
+Add 2 rules, both triggered by Motion events, to the Camera whose ID is provided above(NX_CAMERA_ID)
+
+a) Bookmark with the tag `motion1`  
+b) Make an HTTP request to this http url `http://IP-of-Script:8001/`
+
+
+New motion events should now show up as thumbnails on your Platerecognizer dashboard
+
+
+## Links
+
+NX VMS API Endpoints Documentation
+https://localhost:7001/static/index.html#/developers/api/ec2/cameraThumbnail
+
+NX VMS API Authentication
+https://support.networkoptix.com/hc/en-us/articles/360020373754-Connecting-a-System-to-Nx-Cloud-using-the-API
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,85 +1,119 @@
-"""
-users will need to run a standalone application that the VMS will send the short video to then the application will retrieve a video and forward to Stream SDK or the API cloud.
-
-Configure 2 rules for the camera on the VMS
-a) Bookmark Motion events with a tag
-b) Make an HTTP request to notify about motion events
-Start Server that listens for the HTTP requests from the camera
-When a motion event is received by the server...
-
-
-Auth
-https://support.networkoptix.com/hc/en-us/articles/360020373754-Connecting-a-System-to-Nx-Cloud-using-the-API
-
-Dev API Endpoints
-https://localhost:7001/static/index.html#/developers/api/ec2/cameraThumbnail
-
-
-"""
-
-import cgi
-import json
+import logging
+import sys
+import io
+import os
 import requests
+from requests.auth import HTTPDigestAuth
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from json.decoder import JSONDecodeError
+
+
+LOG_LEVEL = os.environ.get('LOGGING', 'INFO').upper()
+
+logging.basicConfig(
+    stream=sys.stdout,
+    level=LOG_LEVEL,
+    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s'
+)
+
 
 # Constants
-NIX_API_BASE = 'https://localhost:7001/'
-NIX_CAMERA_ID = '420f37f6-8875-6885-9200-11504e61f485'
+NX_BOOKMARK_TAG = 'motion1'
+NX_API_BASE = 'https://192.168.0.14:7001/'
+NX_CAMERA_ID = '420f37f6-8875-6885-9200-11504e61f485'
 
-SYSTEM_LOGIN = "admin"
-SYSTEM_PASSWORD = "NxWitness"
+NX_LOGIN = "username"
+NX_PASSWORD = "password@"
+
+API_TOKEN = '4805bee1222###############################'
 
 # End Constants
+
+auth = HTTPDigestAuth(NX_LOGIN, NX_PASSWORD)
+
+
+def download_bookmarks():
+    logging.info(f'Downloading bookmarks')
+
+    bookmarks_endpoint = f'{NX_API_BASE}ec2/bookmarks'
+    bookmarks_res = requests.get(
+        bookmarks_endpoint,
+        params={
+            'cameraId' : NX_CAMERA_ID,
+            'filter': NX_BOOKMARK_TAG,
+            'sortBy':'startTime',
+            'sortOrder':'desc',
+            'limit':1
+        },
+        auth=auth,
+        verify=False
+    )
+
+    logging.info(f'bookmarks_res: {bookmarks_res}')
+    return bookmarks_res.json()
+
+
+def download_thumbnail(start_timestamp):
+    logging.info(f'Downloading thumbnail after: {start_timestamp}')
+
+    thumbnail_endpoint = f'{NX_API_BASE}ec2/cameraThumbnail'
+
+    thumbnail_res = requests.get(
+        thumbnail_endpoint,
+        params={
+            'cameraId': NX_CAMERA_ID,
+            'time': start_timestamp,
+            'imageFormat': 'png',
+            'method': 'after'
+        },
+        auth=auth,
+        verify=False,
+        stream=True
+    )
+
+    logging.info(f'thumbnail_res: {thumbnail_res}')
+
+    return thumbnail_res
+
 
 
 def main():
     # Retrieve the latest bookmark from the camera to get the motion start timestamp
-    # this done through the API
-    # TODO order by date desc and limit to 1
-    endpoint  = f'{NIX_API_BASE}ec2/bookmarks'
-    params = {
-        'cameraId' : NIX_CAMERA_ID,
-        'filter': 'motion1'
-    }
+    bookmarks = download_bookmarks()
 
+    if len(bookmarks):
+        logging.debug('Bookmarks found.')
+        bookmark = bookmarks[0]
+        start_timestamp = bookmark['startTimeMs']
 
-    res = requests.get(
-        endpoint,
-        params=params,
-        auth=requests.auth.HTTPDigestAuth(SYSTEM_LOGIN, SYSTEM_PASSWORD)
-    )
-
-    events = res.json()
-    if len(events):
-        event = events[0]
-        start_timestamp = event['startTimeMs']
-
-        # Download an image through API by at the start timestamp
-        endpoint2 = f'https://localhost:7001/ec2/cameraThumbnail'
-        params2 = {
-            'cameraId': NIX_CAMERA_ID,
-            'time':start_timestamp,
-            'imageFormat':'png',
-            'method':'after'
-        }
-
-        thumbnail_res = requests.get(
-            endpoint2,
-            params=params2,
-            auth=requests.auth.HTTPDigestAuth(SYSTEM_LOGIN, SYSTEM_PASSWORD)
-        )
-
+        thumbnail_res = download_thumbnail(start_timestamp)
 
         # Send the downloaded image to Snapshot API
+        buffer = io.BytesIO(thumbnail_res.content)
+        buffer.seek(0)
+        plate_reader_response = requests.post(
+            # 'http://localhost:8080/v1/plate-reader/',
+            'http://api.platerecognizer.com/v1/plate-reader',
+            files=dict(upload=buffer),
+            headers={
+                'Authorization': 'Token ' + API_TOKEN,
+            }
+            # data=dict(regions=[],
+            # camera_id='dz1',
+            # config=json.dumps(dict(mode='fast'))
+            # )
+        )
 
+        logging.info(plate_reader_response.text)
+    else:
+
+        logging.info('Bookmarks not found')
 
 
 
 class GetHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        logging.info('HTTP Event Received.')
         main()
-
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b'Event Processed.')
@@ -88,5 +122,5 @@ class GetHandler(BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
     server = HTTPServer(('', 8001), GetHandler)
-    print('Starting server, use <Ctrl-C> to stop')
+    logging.info('Starting server, use <Ctrl-C> to stop')
     server.serve_forever()
